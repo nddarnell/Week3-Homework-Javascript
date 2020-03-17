8fc9c4cf4fa19df53ed17767257d2f7216f0f726
Nathan Darnell <darnell.nathaniel95@gmail.com>
2020-03-16 21:27:16 -0700

    added some js

--- pwgenerator.js
+++ pwgenerator.js
@@ -1,4 +1,16 @@
 function generateStart(){
-    var howManyCharacter = prompt("How many characters would you like your password to have?");
-    
+    var howManyCharacter = prompt("How many characters would you like your password to have? Must be betwen 8 and 128 characters.");
+    if (howManyCharacter < 8) {
+        alert("Must be between 8 and 128 characters.")
+    }
+    else if (howManyCharacter > 128) {
+        alert("Must be between 8 and 128 characters.")
+    }
+    else if (howManyCharacter > 8 < 128) {
+        
+    }
+    var specialCharacter = confirm("Click OK to include special characters")
+    if (specialCharacter != confirm) {
+
+    }
 }
